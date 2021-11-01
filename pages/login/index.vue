588028fcfc2b037642d3887f3f49b931acbcd930
t-east <azmtomya.a127@gmail.com>
2021-11-01 20:58:06 +0900

    loginページ雛形作成

---
+++ pages/login/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <div class="flex justify-center">
+        <div class="my-12 py-8 px-16 bg-gray-100 w-1/2 shadow shadow-2xl border">
+            <h1 class="text-xl font-bold mb-8 text-yellow-600">
+                ユーザ登録
+            </h1>
+            <div>
+              <InputForm
+                label="asdf"
+                placeholder="asdf"
+                msg="sdfssss"
+              />
+            </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import InputForm from '~/components/molecules/InputForm.vue'
+import AtomButton from '~/components/atoms/AtomButton.vue'
+export default Vue.extend({
+  components: {
+    AtomButton,
+    InputForm
+  }
+
+})
+</script>
