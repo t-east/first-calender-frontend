7919d942fe28980a454969af7b26144d13963a47
t-east <azmtomya.a127@gmail.com>
2022-01-31 14:30:26 +0900

    ユーザ登録機能追加

--- components/organism/modals/BaseModal.vue
+++ components/organism/modals/BaseModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed inset-0 rounded-xl bg-opacity-75 bg-gray-400 flex shadow-lg items-center justify-center z-10">
+  <div class="fixed inset-0 bg-opacity-30 bg-black flex shadow-lg items-center justify-center z-10">
     <div :class="`bg-white text-black opacity-100 py-4 px-12 w-${$props.size} h-${$props.size} rounded-2xl relative`">
       <slot />
     </div>
